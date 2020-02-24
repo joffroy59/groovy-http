beb53a606d90adadca722ec7eff54ff17b5a23a5
Jerome OFFROY <jerome.offroy@gmail.com>
2020-02-24 22:52:08 +0100

    feat(mavenrepository): extract

--- HttpConnect/src/AccessMavenRepository.groovy
+++ HttpConnect/src/AccessMavenRepository.groovy
@@ -1,4 +1,5 @@
-import javax.net.ssl.HttpsURLConnection
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
 
 import groovy.xml.XmlUtil
 
@@ -28,6 +29,33 @@ class MyConnect {
 		//TODO use a classifier
 		def url = getUrl(groupId, artefactId, version, filename)
 		download(url,outputFolder)
+	}
+	
+	public extractJar(downloadFolder, filename) {
+		println "exrtact ${filename} to ${downloadFolder+File.separator}}"
+		def jar = new JarFile(filename)
+		def jarFile = new File(jar.name)
+		def distDir = "${downloadFolder+File.separator}"
+		def libName = jarFile.name.with {it.take(it.lastIndexOf('.'))}
+		new File("${distDir+libName}").with {
+			if (!it.exists()) it.mkdirs()
+		}
+		distDir += libName+File.separator
+		for (JarEntry file in jar.entries()){
+			def f = new File("${distDir+file.name}")
+			if (file.isDirectory()){
+				f.mkdir()
+				continue
+			}
+			def is = jar.getInputStream(file)
+			f.withOutputStream { def stream ->
+				while (is.available()>0)
+					stream.write(is.read())
+			}
+			is.close()
+		}
+		jar.close()
+		println "exrtacted to: ${downloadFolder}"
 	}
 	
 	private void download(def address, def outputFolder) {
@@ -65,8 +93,10 @@ groupId="net.sourceforge.nekohtml"
 artefactId="nekohtml"
 version="1.9.22"
 type="jar"
-downloadFolder="./download"
+downloadFolder="download"
 filename=myConnect.getMavenFilename(artefactId, version, type)
 println "Download a Maven artefact : ${groupId}:${artefactId}:${version} to :${downloadFolder}"
 myConnect.downloadMavenartefact(groupId, artefactId, version, downloadFolder, filename)
 println "Downloaded ${filename}"
+
+myConnect.extractJar("${downloadFolder+File.separator}", "${downloadFolder+File.separator+filename}")
